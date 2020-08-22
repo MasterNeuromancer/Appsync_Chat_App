549068ee656c09b4ea6e9efe712f895499bc03a3
Ivan Trejo <ivandev@Ivans-MacBook-Pro.local>
2020-08-22 16:28:44 -0700

    got initial login to display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { View, Image, StyleSheet } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import { Hub, Auth } from 'aws-amplify'
-import { withAuthenticator } from 'aws-amplify-react-native'
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { withAuthenticator } from 'aws-amplify-react-native';
 
-export default () => {
-  const { signedIn, setSignedIn } = useState(null);
-  useEffect(() => async {});
-  const AppComponent = withAuthenticator(App, 
-    {
-      signUpConfig: {
-        hiddenDefaults: ['phone_number']
-      }
-    },
-    null, null, theme
-    );
+const App = () => {
+
   return (
-    <View style={styles.appContainer}>
-      {!this.state.signedIn && <Logo />}
-      <AppComponent {...this.props} />
+    <View style={styles.container}>
+        <Text>Hello From the App Page!</Text>
     </View>
-  );
-};
+  )
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', padding: 20 },
+  todo: {  marginBottom: 15 },
+  input: { height: 50, backgroundColor: '#ddd', marginBottom: 10, padding: 8 },
+  todoName: { fontSize: 18 }
+})
+
+export default withAuthenticator(App);
