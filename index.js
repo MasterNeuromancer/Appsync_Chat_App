0b6988e25e5d1510cb9c537ff183f0ea848e1674
Neuromancer93 <itrejonumber1@gmail.com>
2020-08-23 18:47:47 -0700

    added safeareaprovider, switched to logbox

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-/**
- * @format
- */
+import { AppRegistry, LogBox } from 'react-native';
 
-import {AppRegistry, YellowBox} from 'react-native';
 import App from './src/App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
-YellowBox.ignoreWarnings([
+LogBox.ignoreLogs([
   'Possible Unhandled Promise Rejection',
   'Remote debugger',
 ]);
--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './Components/RootNavigator';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <RootNavigator/>
-    </NavigationContainer>
-  )
-}
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <RootNavigator/>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+};
 
 export default withAuthenticator(App);
