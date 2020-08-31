10d2890bbc879974128f1d1fdccbb60ef83451fe
Neuromancer93 <itrejonumber1@gmail.com>
2020-08-30 23:57:42 -0700

    correctly checking for user data

--- src/lib/User.js
+++ src/lib/User.js
@@ -4,15 +4,16 @@ import { getUser } from '../graphql/queries';
 import { createUser } from '../graphql/mutations';
 
 export const useUserData = () => {
-    // const [userData, setUserData] = useState(null);
+    const [userData, setUserData] = useState(null);
+    const [createUserData, setCreateUserData] = useState(null);
     const [authUser, setAuthUser] = useState(null);
+    const [loading, setLoading] = useState(null);
 
     const checkAuthForUser = () => {
         const authResponse = Auth.user.username;
         
         if(authResponse){
             setAuthUser(authResponse);
-            console.log('authresponse in hook =====>', authResponse);
         } 
     };
 
@@ -20,11 +21,34 @@ export const useUserData = () => {
         checkAuthForUser();
     }, [checkAuthForUser]);
 
-    console.log('authUser in hook ', authUser);
+    useEffect(() => {
+        async function fetchUserData(authUser) {
+            try {
+                setLoading(true);
+                console.log('auth user in try catch', authUser);
+                const userResponse = await API.graphql(graphqlOperation(getUser, { id: authUser } ));
+                console.log('user response in hook ======>', userResponse);
+                const { user } = userResponse.data;
+                if (!user) {
+                    // create user
+                    setUserData('no user found');
+                    setLoading(false);
+                } else {
+                    console.log('user data in try catch, hook block ================>', user);
+                    setUserData(user);
+                    setLoading(false);
+                }
+            } catch (error) {
+                console.log('error', error);
+                setLoading(false);
+            }
+        }
 
-    if (authUser) {
-        return authUser;
-    } else {
-        return 'no user found';
-    }
+        if (authUser) {
+            console.log('got to auth user check this is it', authUser);
+            fetchUserData(authUser);
+        }
+    }, [authUser]);
+
+    return userData;
 };
